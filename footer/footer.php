fc1bbfe93f8e6960950a2e4d8a5e6030b0001ce4
Shivam Chaudhary <32065724+shivam-199@users.noreply.github.com>
2019-02-09 17:58:47 +0530

    Footer

---
+++ footer/footer.php
@@ -0,0 +1,34 @@
+<html>
+    <head>
+        <title>Leadership</title>
+        <script src="min/jquery-1.10.2.min.js"></script>	
+        <link rel="stylesheet" href="css/style_block7.css">
+        <link rel="stylesheet" href="min/bootstrap.min.css">   
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css">  
+        <link rel="stylesheet" href="min/font-awesome-animation.min.css">	
+		        
+        
+    </head>
+    <body>
+        <div class="bod">
+            <footer>
+                <div class="all_content">
+
+                    <div class="contacts">
+                        <p class="emails"><i class="det fas fa-envelope"></i>&nbsp admin@leadershipsummit.co.in</p>
+                        <p class="contact"><i class="det fas fa-phone-square"></i>&nbsp 91xxxxxxxx</p>
+                        <p class="location"><i class="det fas fa-map-marker-alt"></i>&nbsp St. Xavier's College Autonomous</p>
+
+                    </div>
+                    <div class="iconss" >
+                        <a href="#"><i class="ic fab fa-facebook-f"   ></i></a>
+                        <a href="#"><i class="ic fab fa-instagram  "></i></a>
+                        <a href="#"><i class="ic fab fa-linkedin-in  "></i></a>
+                        <a href="#"><i class="ic fab fa-twitter "></i></i></a>
+                    </div>                    
+                </div>          
+                    
+            </footer>
+        </div>
+    </body>
+</html>
