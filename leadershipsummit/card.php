8e4ba7d9d8f8fe5a27136790fe6384fe525dae03
Shivam Chaudhary <32065724+shivam-199@users.noreply.github.com>
2019-04-29 23:32:03 +0530

    Please let us work from here
    We start from here and build up.

---
+++ leadershipsummit/card.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Card</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/card.css">
+    <link rel="stylesheet" href="min/bootstrap.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css">  
+    <link rel="stylesheet" href="min/font-awesome-animation.min.css">	
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css">
+    <script src="main.js"></script>
+</head>
+<body>
+    <section>
+        <div class="container-fluid">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-4">
+                        <div class="card text-center">
+                            <div class="title">
+                                <i class="fa fa-paper-plane" aria-hidden="true"></i>
+                                <h2>Basic</h2>
+                            </div>
+                            <div class="price">
+                                <h4><sup>$</sup>25</h4>
+                            </div>
+                            <div class="option">
+                                <ul>
+                                    <li><i class="fa fa-check" aria-hidden="true"></i> 10 GB Space</li>
+                                    <li><i class="fa fa-check" aria-hidden="true"></i> 3 Domain Names</li>
+                                    <li><i class="fa fa-check" aria-hidden="true"></i> 20 Email Addresses</li>
+                                    <li><i class="fa fa-times" aria-hidden="true"></i> Live Support</li>
+                                </ul>
+                            </div>
+                            <a href="#">Order</a>
+                        </div>
+                    </div>
+                    <div class="col-sm-4">
+                        <div class="card text-center">
+                            <div class="title">
+                                <i class="fa fa-paper-plane" aria-hidden="true"></i>
+                                <h2>Standard</h2>
+                            </div>
+                            <div class="price">
+                                <h4><sup>$</sup>25</h4>
+                            </div>
+                            <div class="option">
+                                <ul>
+                                    <li><i class="fa fa-check" aria-hidden="true"></i> 10 GB Space</li>
+                                    <li><i class="fa fa-check" aria-hidden="true"></i> 3 Domain Names</li>
+                                    <li><i class="fa fa-check" aria-hidden="true"></i> 20 Email Addresses</li>
+                                    <li><i class="fa fa-times" aria-hidden="true"></i> Live Support</li>
+                                </ul>
+                            </div>
+                            <a href="#">Order</a>
+                        </div>
+                    </div>
+                    <div class="col-sm-4">
+                        <div class="card text-center">
+                            <div class="title">
+                                <i class="fa fa-rocket" aria-hidden="true"></i>
+                                <h2>Premium</h2>
+                            </div>
+                            <div class="price">
+                                <h4><sup>$</sup>25</h4>
+                            </div>
+                            <div class="option">
+                                <ul>
+                                    <li><i class="fa fa-check" aria-hidden="true"></i> 10 GB Space</li>
+                                    <li><i class="fa fa-check" aria-hidden="true"></i> 3 Domain Names</li>
+                                    <li><i class="fa fa-check" aria-hidden="true"></i> 20 Email Addresses</li>
+                                    <li><i class="fa fa-times" aria-hidden="true"></i> Live Support</li>
+                                </ul>
+                            </div>
+                            <a href="#">Order</a>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        
+        </div>
+    </section>
+    
+</body>
+</html>
