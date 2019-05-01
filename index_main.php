78321d8709fee827b101e32fada03bc4131eaabe
Shivam Chaudhary <32065724+shivam-199@users.noreply.github.com>
2019-05-02 00:01:42 +0530

    linking
    Co-Authored-By: Sonagra Sumit <sonagrasumit@gmail.com>

--- index_main.php
+++ index_main.php
@@ -69,8 +69,8 @@ $(window).on("resize", function () {
 <div class="grid-container">
 
 <div class="topnav" id="Topnav" style = "right: -3px">
-    <a href="#item1" style = "color: #916b2f">Home</a>
-    <a href="#item2" style = "color: #916b2f">About Us</a>
+    <a href="index_main.php" style = "color: #916b2f">Home</a>
+    <a href="aboutUs.php" style = "color: #916b2f">About Us</a>
     <a href="speakers.php" style = "color: #916b2f">Speakers</a>
     <a href="#" style = "color: #916b2f">Sponsors</a>
     <a href="#item6" style = "color: #916b2f; border-right: none;">Contact</a>
